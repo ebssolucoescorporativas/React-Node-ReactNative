609472d863d3f08ff91a08230b37d411502d66b4
Eduardo Bueno da Silva <eduardo.bueno@e2ti.com.br>
2021-08-23 17:08:41 -0400

    add files to project

--- back-end/app.js
+++ back-end/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+
+app.use(express.json());
  
-app.get('/', async (req, res) => {
-  res.send('Bem vindo Eduardo!!');
+app.get("/", async (req, res) => {
+  res.send('Bem vindo Eduardo!!! ');
 });
  
+app.post("/cad-produto", async (req, res) => {
+  console.log(req.body);
+  res.send('Cadastrar produto!!! ');
+});
+
 app.listen(8080, ()=> {
     console.log("Servidor iniciado na porta 8080: http://localhost/8080");
 });
